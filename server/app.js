b8d7b0c0bd608861f79bbcbb86266d7cf0bca687
Shubhanker Singh <shubh.folk004@gmail.com>
2021-12-22 07:23:53 -0600

    Set up everything to deploy on heroku.

--- server/app.js
+++ server/app.js
@@ -59,6 +59,14 @@ app.use((err, req, res, next) => {
 });
 
 
+app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.get('/*',(req, res)=>{
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  });
+}
+
 const port = process.env.PORT || 3001;
 
 mongoose
